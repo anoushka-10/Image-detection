0ce3a93d49e14b42c3e27bffb78d7c992ae3b957
anoushka-10 <anoushkag02@gmail.com>
2024-08-01 19:57:23 +0530

    app.py updated

--- app.py
+++ app.py
@@ -12,22 +12,21 @@ def get_vision_client():
     secrets = st.secrets["GOOGLE_APPLICATION_CREDENTIALS"]
     
     # Convert TOML values to dictionary
-    credentials_info = {
-        "type": secrets["type"],
-        "project_id": secrets["project_id"],
-        "private_key_id": secrets["private_key_id"],
-        "private_key": secrets["private_key"].replace("\\n", "\n"),
-        "client_email": secrets["client_email"],
-        "client_id": secrets["client_id"],
-        "auth_uri": secrets["auth_uri"],
-        "token_uri": secrets["token_uri"],
-        "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": secrets["client_x509_cert_url"],
-        "universe_domain": secrets["universe_domain"]
-    }
+    # credentials_info = {
+    #     "type": secrets["type"],
+    #     "project_id": secrets["project_id"],
+    #     "private_key_id": secrets["private_key_id"],
+    #     "private_key": secrets["private_key"].replace("\\n", "\n"),
+    #     "client_email": secrets["client_email"],
+    #     "client_id": secrets["client_id"],
+    #     "auth_uri": secrets["auth_uri"],
+    #     "token_uri": secrets["token_uri"],
+    #     "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
+    #     "client_x509_cert_url": secrets["client_x509_cert_url"],
+    #     "universe_domain": secrets["universe_domain"]
+    # }
     
-    credentials = service_account.Credentials.from_service_account_info(credentials_info)
-    client = vision.ImageAnnotatorClient(credentials=credentials)
+    client = vision.ImageAnnotatorClient(credentials=secrets)
     return client
 
 client = get_vision_client()
